80a929a1fc3b18960e7ce83ff59d0b0fdc0d4ac2
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-02-01 20:28:25 +0200

    Add files via upload

---
+++ Working with Abstraction/Exercise/CardRank_02/Main.java
@@ -0,0 +1,10 @@
+package WorkingWithAbstractionExercise.CardRank_02;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card Ranks:");
+        for(CardRank cardRank : CardRank.values()){
+            System.out.printf("Ordinal value: %d; Name value: %s%n", cardRank.ordinal(), cardRank.name());
+        }
+    }
+}
