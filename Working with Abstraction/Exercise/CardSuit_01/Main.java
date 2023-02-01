12fe414089ba8b219f0168eb469dcac5870e2df0
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-02-01 20:28:11 +0200

    Add files via upload

---
+++ Working with Abstraction/Exercise/CardSuit_01/Main.java
@@ -0,0 +1,10 @@
+package WorkingWithAbstractionExercise.CardSuit_01;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Card Suits:");
+        for(CardSuit cardSuit : CardSuit.values()){
+            System.out.printf("Ordinal value: %d; Name value: %s%n", cardSuit.ordinal(), cardSuit.name());
+        }
+    }
+}
