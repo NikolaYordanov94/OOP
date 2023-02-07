05116535562d665ed08aeab855b50de8c4c1e901
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-02-07 22:02:31 +0200

    Add files via upload

---
+++ Encapsulation/Lab/SalaryIncrease_02/Person.java
@@ -0,0 +1,58 @@
+package Encapsulation.SalaryIncrease_02;
+
+public class Person {
+    private String firstName;
+    private String lastName;
+    private int age;
+    private double salary;
+
+    public Person(String firstName, String lastName, int age, double salary) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.salary = salary;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getSalary() {
+        return salary;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public void setSalary(double salary) {
+        this.salary = salary;
+    }
+
+    public void increaseSalary(double bonus) {
+        double bonusReduced = age < 30 ? bonus / 2 : bonus;
+
+        double factor = 1.00 + bonusReduced / 100;
+        salary = salary * factor;
+    }
+    @Override
+    public String toString(){
+        return String.format("%s %s gets %.1f leva", firstName, lastName, salary);
+    }
+}
