a4b0f7deb0858154fb2b971eff09452cc2580d98
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-02-22 21:59:46 +0200

    Add files via upload

---
+++ Inheritance/Lab/MultipleInheritance_02/Animal.java
@@ -0,0 +1,7 @@
+package Inheritance.MultipleInheritance_02;
+
+public class Animal {
+    public void eat(){
+        System.out.println("eating");
+    }
+}
---
+++ Inheritance/Lab/MultipleInheritance_02/Dog.java
@@ -0,0 +1,7 @@
+package Inheritance.MultipleInheritance_02;
+
+public class Dog extends Animal{
+    public void bark(){
+        System.out.println("barking");
+    }
+}
