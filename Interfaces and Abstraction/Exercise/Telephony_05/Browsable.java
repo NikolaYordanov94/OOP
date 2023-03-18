1b212c5e16d5a2b0997705fb2ecb381172190dbc
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-03-18 22:55:59 +0200

    Initial Commit

---
+++ Interfaces and Abstraction/Exercise/Telephony_05/Browsable.java
@@ -0,0 +1,6 @@
+package InterfacesAndAbstractionExercises.Telephony_05;
+
+public interface Browsable {
+    String browse();
+
+}
---
+++ Interfaces and Abstraction/Exercise/Telephony_05/Callable.java
@@ -0,0 +1,6 @@
+package InterfacesAndAbstractionExercises.Telephony_05;
+
+public interface Callable {
+    String call();
+
+}
