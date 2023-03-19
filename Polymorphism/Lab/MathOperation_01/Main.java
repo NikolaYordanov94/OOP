e575f832bfe8ef91efdf2cd96e17cbd68808119f
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-03-20 00:59:35 +0200

    Initial Commit

---
+++ Polymorphism/Lab/MathOperation_01/Main.java
@@ -0,0 +1,12 @@
+package Polymorphism.MathOperation_01;
+import java.io.IOException;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println(MathOperation.add(2, 2));
+        System.out.println(MathOperation.add(3, 3, 3));
+        System.out.println(MathOperation.add(4, 4, 4, 4));
+    }
+
+}
