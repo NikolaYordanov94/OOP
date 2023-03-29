057bb81269d7271caf11ef25fa746e46170fd9c6
Nikola Yordanov <106193855+NikolaYordanov94@users.noreply.github.com>
2023-03-30 00:16:02 +0300

    Initial Commit

---
+++ SquareRoot_02.java
@@ -0,0 +1,14 @@
+package ExceptionsAndErrorHandling;
+import java.util.Scanner;
+
+public class SquareRoot_02 {
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+
+
+
+
+    }
+
+}
